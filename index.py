34428b6ababc976b04163a17b8c62b64980a5a88
snakecy <andy21.chow@gmail.com>
2023-03-03 20:55:18 +0800

    update

--- index.py
+++ index.py
@@ -36,4 +36,4 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="app:app", host="0.0.0.0", port=8080, reload=False)
+    uvicorn.run(app="index:app", host="0.0.0.0", port=8080, reload=False)
