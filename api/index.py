05bc2ea5d0fde70f5b2e318dade46a1a74041c1a
snakecy <andy21.chow@gmail.com>
2023-03-03 21:20:58 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,37 +1,12 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# ================================================
+from http.server import BaseHTTPRequestHandler
+from cowpy import cow
 
-# @FileName          : app.py
-# @Author            : zhoushaolong
-# @Email             : zhoushaolong@haier.com
-# @CreatedTime       : 2023-03-03 20:08
-# @ModifiedTime      :
+class handler(BaseHTTPRequestHandler):
 
-# ================================================
-"""
-# Decription
-
-"""
-
-import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-
-@app.get("/")
-async def query():
-    return "hello world"
-
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        self.wfile.write(message.encode())
+        return
